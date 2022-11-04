cf22b99c52037c5a9c677a76845068f090a5122a
Kurt Braun <kbraun@kbraun.dev>
2022-11-04 08:22:12 -0500

    Day 17 complete

--- src/bin/day-17/main.rs
+++ src/bin/day-17/main.rs
@@ -1,25 +1,69 @@
 #![feature(exclusive_range_pattern)]
-use aoc_2015::lines_for_day_test;
+use std::{collections::HashSet, vec};
+
+use aoc_2015::lines_for_day;
 
 fn main() {
-    let mut lines = lines_for_day_test("day-17")
+    let mut lines = lines_for_day("day-17")
         .into_iter()
         .map(|l| l.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
     lines.sort();
 
-    println!("{:?}", sum_to(&lines, 25));
+    let containers = lines
+        .into_iter()
+        .enumerate()
+        .map(|(id, capacity)| Container { id, capacity })
+        .collect::<Vec<Container>>();
+
+    let mut set = HashSet::new();
+
+    sum_to(&containers, 150, &mut set, vec![]);
+
+    println!("Part 1: {}", set.len());
+
+    let min = set.iter().map(Vec::len).min().unwrap_or(0);
+
+    println!(
+        "Part 2: {}",
+        set.iter().map(Vec::len).filter(|l| *l == min).count()
+    );
 }
 
-fn sum_to(rest: &[i32], sum: i32) -> i32 {
-    dbg!(rest, sum);
+fn sum_to(
+    rest: &[Container],
+    sum: i32,
+    set: &mut HashSet<Vec<Container>>,
+    path: Vec<Container>,
+) -> i32 {
     match (rest, sum) {
-        (_, 0) => 1,
-        ([rest], sum) if *rest == sum => 1,
+        (_, 0) => {
+            set.insert(path);
+            1
+        }
+        ([rest], sum) if rest.capacity == sum => {
+            let mut path_with = path.clone();
+            path_with.push(*rest);
+            set.insert(path_with);
+            1
+        }
         (_, i32::MIN..0) | ([], _) | ([_], _) => 0,
-        (rest, sum) => (0..rest.len() - 1)
-            .map(|i| sum_to(&rest[i + 1..], sum - rest[i]))
-            .sum(),
+        (rest, sum) => {
+            (0..rest.len() - 1)
+                .map(|i| {
+                    let mut path_with = path.clone();
+                    path_with.push(rest[i]);
+                    sum_to(&rest[i + 1..], sum - rest[i].capacity, set, path_with)
+                })
+                .sum::<i32>()
+                + sum_to(&rest[1..], sum, set, path.clone())
+        }
     }
+}
+
+#[derive(Debug, PartialEq, Hash, Clone, Copy, Eq)]
+struct Container {
+    id: usize,
+    capacity: i32,
 }
